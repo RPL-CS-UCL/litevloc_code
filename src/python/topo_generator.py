3fe35b22a67a01a24e2824cb43e835825350a653
gogojjh <jiaojh1994@gmail.com>
2024-06-29 11:00:41 +0100

    add the usage of pycpptools

--- src/python/topo_generator.py
+++ src/python/topo_generator.py
@@ -20,6 +20,8 @@
 
 import os
 import argparse
+import numpy as np
+
 import rospy
 import message_filters
 from nav_msgs.msg import Odometry
@@ -28,14 +30,10 @@ from geometry_msgs.msg import TransformStamped
 from tf import TransformListener
 from cv_bridge import CvBridge
 import cv2
-import numpy as np
-import argparse
-from scipy.spatial.transform import Rotation as R
 from sensor_msgs.msg import Image, CompressedImage
-import math
 
-# Initialize the node
-rospy.init_node('topo_generator')
+from pycpptools.python.utils_ros.tools_ros_msg_conversion import convert_rosodom_to_vec
+from pycpptools.python.utils.ros.tools_eigen import add_gaussian_noise_to_pose, compute_relative_dis
 
 class TopoGenerator:
 	def __init__(self):
@@ -48,24 +46,26 @@ class TopoGenerator:
 		parser.add_argument('--topo_int_trans', type=float, default=3.0)
 		parser.add_argument('--topo_int_rot', type=float, default=45.0)
 		parser.add_argument('--data_path', type=str, default='/tmp', help='data_path')
-		parser.add_argument('--dataset_type', type=str, default='matterport3d', help='matterport3d, anymal')	
+		parser.add_argument('--dataset_type', type=str, default='matterport3d', help='matterport3d, anymal')
 		self.args = parser.parse_args()
 
-		# ROS Subscriber
 		if self.args.dataset_type == 'anymal':
-			rgb_sub = message_filters.Subscriber(self.args.rgb_topic, CompressedImage)
-			depth_sub = message_filters.Subscriber(self.args.depth_topic, CompressedImage)
-			semantic_sub = message_filters.Subscriber(self.args.semantic_topic, Image)
-			camera_odom_sub = message_filters.Subscriber(self.args.odometry_topic, Odometry)
-			ts = message_filters.ApproximateTimeSynchronizer([rgb_sub, depth_sub, semantic_sub, camera_odom_sub], 100, 0.1, allow_headerless=True)
-			ts.registerCallback(self.image_callback)
+			self.RGB_IMAGE_TYPE = CompressedImage
+			self.RGB_CV_FUNCTION = CvBridge().compressed_imgmsg_to_cv2
 		else:
-			rgb_sub = message_filters.Subscriber(self.args.rgb_topic, Image)
-			depth_sub = message_filters.Subscriber(self.args.depth_topic, Image)
-			semantic_sub = message_filters.Subscriber(self.args.semantic_topic, Image)
-			camera_odom_sub = message_filters.Subscriber(self.args.odometry_topic, Odometry)
-			ts = message_filters.ApproximateTimeSynchronizer([rgb_sub, depth_sub, semantic_sub, camera_odom_sub], 100, 0.1, allow_headerless=True)
-			ts.registerCallback(self.image_callback)
+			self.RGB_IMAGE_TYPE = Image
+			self.RGB_CV_FUNCTION = CvBridge().imgmsg_to_cv2
+
+		# Initialize the node
+		rospy.init_node('topo_generator')
+
+		# ROS Subscriber
+		rgb_sub = message_filters.Subscriber(self.args.rgb_topic, self.RGB_IMAGE_TYPE)
+		depth_sub = message_filters.Subscriber(self.args.depth_topic, Image)
+		semantic_sub = message_filters.Subscriber(self.args.semantic_topic, self.RGB_IMAGE_TYPE)
+		camera_odom_sub = message_filters.Subscriber(self.args.odometry_topic, Odometry)
+		ts = message_filters.ApproximateTimeSynchronizer([rgb_sub, depth_sub, semantic_sub, camera_odom_sub], 100, 0.1, allow_headerless=True)
+		ts.registerCallback(self.image_callback)
 
 		imu_sub = rospy.Subscriber(self.args.imu_topic, Imu, self.imu_callback)
 
@@ -81,6 +81,8 @@ class TopoGenerator:
 		# Parameter
 		self.frame_id = 0
 		self.map_id = 0
+
+		# Files
 		self.pose_file = open('{}/camera_pose.txt'.format(self.args.data_path), 'a')
 		self.noisy_pose_file = open('{}/noisy_camera_pose.txt'.format(self.args.data_path), 'a')
 		self.topomap_pose_file = open('{}/topo_map/camera_pose.txt'.format(self.args.data_path), 'a')
@@ -93,45 +95,40 @@ class TopoGenerator:
 		print('image_callback: {}'.format(self.frame_id))
 		timestamp = rgb_image.header.stamp
 
-		trans = np.array([camera_odom.pose.pose.position.x, 
-											camera_odom.pose.pose.position.y, 
-											camera_odom.pose.pose.position.z])
-		rot = np.array([camera_odom.pose.pose.orientation.w, 
-										camera_odom.pose.pose.orientation.x,
-										camera_odom.pose.pose.orientation.y,
-										camera_odom.pose.pose.orientation.z])			
-		self.save_pose(self.pose_file, timestamp, trans, rot)
+		trans, quat = convert_rosodom_to_vec(camera_odom)
+		self.save_pose(self.pose_file, timestamp, trans, quat)
 		
-		noisy_trans, noisy_rot = self.add_pose_noise(trans, rot)
-		self.save_pose(self.noisy_pose_file, timestamp, noisy_trans, noisy_rot)
+		noisy_trans, noisy_quat = add_gaussian_noise_to_pose(trans, quat)
+		self.save_pose(self.noisy_pose_file, timestamp, noisy_trans, noisy_quat)
 
 		bridge = CvBridge()
-		cv_image = bridge.imgmsg_to_cv2(rgb_image, "bgr8")
+		cv_image = self.RGB_CV_FUNCTION(rgb_image, "bgr8")
 		cv2.imwrite('{}/rgb_image/{:06d}.png'.format(self.args.data_path, self.frame_id), cv_image)
+
 		cv_image = bridge.imgmsg_to_cv2(depth_image, "mono8")
 		cv2.imwrite('{}/depth_image/{:06d}.png'.format(self.args.data_path, self.frame_id), cv_image)
-		cv_image = bridge.imgmsg_to_cv2(semantic_image, "bgr8")
+
+		cv_image = self.RGB_CV_FUNCTION(semantic_image, "bgr8")
 		cv2.imwrite('{}/semantic_image/{:06d}.png'.format(self.args.data_path, self.frame_id), cv_image)
 
-		curr_t = np.array([trans[0], trans[1], trans[2]])
-		curr_quat = np.array([rot[0], rot[1], rot[2], rot[3]])
-		rel_t, rel_angle = self.compute_relative_dis(self.last_t, self.last_quat, curr_t, curr_quat)
-		if rel_t > self.args.topo_int_trans or rel_angle > self.args.topo_int_rot:
-			print('Save topo map: {:3f}, {:3f}'.format(rel_t, rel_angle))
-			self.save_pose(self.topomap_pose_file, timestamp, trans, rot)
-			cv_image = bridge.imgmsg_to_cv2(rgb_image, "bgr8")
+		dis_t, dis_angle = compute_relative_dis(self.last_t, self.last_quat, trans, quat)
+		if dis_t > self.args.topo_int_trans or dis_angle > self.args.topo_int_quat:
+			print('Save topo map: {:3f}, {:3f}'.format(dis_t, dis_angle))
+			self.save_pose(self.topomap_pose_file, timestamp, trans, quat)
+
+			cv_image = self.RGB_CV_FUNCTION(rgb_image, "bgr8")
 			cv2.imwrite('{}/topo_map/rgb_image/{:06d}.png'.format(self.args.data_path, self.map_id), cv_image)
+
 			cv_image = bridge.imgmsg_to_cv2(depth_image, "mono8")
 			cv2.imwrite('{}/topo_map/depth_image/{:06d}.png'.format(self.args.data_path, self.map_id), cv_image)
-			cv_image = bridge.imgmsg_to_cv2(semantic_image, "bgr8")
+
+			cv_image = self.RGB_CV_FUNCTION(semantic_image, "bgr8")
 			cv2.imwrite('{}/topo_map/semantic_image/{:06d}.png'.format(self.args.data_path, self.map_id), cv_image)
-			self.last_t, self.last_quat = curr_t, curr_quat
+
+			self.last_t, self.last_quat = trans, quat
 			self.map_id += 1
 
 		self.frame_id += 1
-
-	def odom_callback(self, msg):
-		pass
 
 	def imu_callback(self, msg):
 		line = '{:9f} {:3f} {:3f} {:3f} {:3f} {:3f} {:3f}\n'.format(
@@ -155,24 +152,6 @@ class TopoGenerator:
 		os.makedirs('{}/topo_map/rgb_image'.format(self.args.data_path), exist_ok=True)
 		os.makedirs('{}/topo_map/depth_image'.format(self.args.data_path), exist_ok=True)
 		os.makedirs('{}/topo_map/semantic_image'.format(self.args.data_path), exist_ok=True)
-
-	def compute_relative_dis(self, last_t, last_quat, curr_t, curr_quat):
-		rot1 = R.from_quat(np.roll(last_quat, -1)) # [qw qx qy qz] -> [qx qy qz qw]
-		rot2 = R.from_quat(np.roll(curr_quat, -1))   
-		rel_rot = rot2 * rot1.inv()
-		rel_angle = np.linalg.norm(rel_rot.as_euler('xyz', degrees=True))
-		rel_trans = np.linalg.norm(rot1.inv().apply(last_t - curr_t))
-		return rel_trans, rel_angle
-
-	def add_pose_noise(self, trans, quat, mean=0, stddev=0.1):
-		noisy_trans = [x + np.random.normal(mean, stddev) for x in trans]
-		rot = R.from_quat(np.roll(quat, -1))
-
-		euler = rot.as_euler('xyz', degrees=False)
-		noisy_euler = [x + np.random.normal(mean, stddev) for x in euler]
-		noisy_rot = R.from_euler('xyz', noisy_euler, degrees=False)
-		noisy_quat = np.roll(noisy_rot.as_quat(), 1)
-		return noisy_trans, noisy_quat
 	
 	def save_pose(self, file, timestamp, trans, rot):
 		line = '{:9f} {:3f} {:3f} {:3f} {:3f} {:3f} {:3f}\n'.format(
