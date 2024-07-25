039406f0adc9ddbff330f35a12608dde464208e1
gogojjh <jiaojh1994@gmail.com>
2024-07-25 10:30:31 +0100

    add demo_vpr and demo_image_matching

--- python/utils/utils_image.py
+++ python/utils/utils_image.py
@@ -6,47 +6,55 @@ from PIL import Image
 import numpy as np
 import logging
 
+
 def load_rgb_image(
-  path: Union[str, Path], resize: Union[int, Tuple] = None, rot_angle: float = 0,
-  normalized: bool = False
+    path: Union[str, Path],
+    resize: Union[int, Tuple] = None,
+    rot_angle: float = 0,
+    normalized: bool = False,
 ) -> torch.Tensor:
-  if isinstance(resize, int):
-    resize = (resize, resize)
-  # Set up transformations: - Convert to tensor, - Normalize, - Resize
-  transformations = [tfm.ToTensor()]
-  if normalized:
-    transformations.append(
-      tfm.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]))
-  if resize:
-    transformations.append(tfm.Resize(size=resize, antialias=True))
-  transform = tfm.Compose(transformations)
+    if isinstance(resize, int):
+        resize = (resize, resize)
+    # Set up transformations: - Convert to tensor, - Normalize, - Resize
+    transformations = [tfm.ToTensor()]
+    if normalized:
+        transformations.append(
+            tfm.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        )
+    if resize:
+        transformations.append(tfm.Resize(size=resize, antialias=True))
+    transform = tfm.Compose(transformations)
 
-  # Load image and apply transformation
-  pil_img = Image.open(path).convert("RGB")
-  tensor_size1 = (pil_img.size[1], pil_img.size[0])
-  img = transform(np.array(pil_img))
-  tensor_size2 = img.shape
+    # Load image and apply transformation
+    pil_img = Image.open(path).convert("RGB")
+    tensor_size1 = (pil_img.size[1], pil_img.size[0])
 
-  logging.debug(f' - adding {path} with resolution {tensor_size1} --> {tensor_size2}')
-  return img
+    img = transform(np.array(pil_img))
+    tensor_size2 = img.shape
+
+    logging.debug(f" - adding {path} with resolution {tensor_size1} --> {tensor_size2}")
+    return img
+
 
 def load_depth_image(
-  path: Union[str, Path], resize: Union[int, Tuple] = None, rot_angle: float = 0,
-  depth_scale = 0.001
+    path: Union[str, Path],
+    resize: Union[int, Tuple] = None,
+    rot_angle: float = 0,
+    depth_scale=0.001,
 ) -> torch.Tensor:
-  if isinstance(resize, int):
-    resize = (resize, resize)
-  # Set up transformations: - Convert to tensor, - Resize
-  transformations = [tfm.ToTensor()]
-  if resize:
-    transformations.append(tfm.Resize(size=resize, antialias=True))
-  transform = tfm.Compose(transformations)
+    if isinstance(resize, int):
+        resize = (resize, resize)
+    # Set up transformations: - Convert to tensor, - Resize
+    transformations = [tfm.ToTensor()]
+    if resize:
+        transformations.append(tfm.Resize(size=resize, antialias=True))
+    transform = tfm.Compose(transformations)
 
-  # Load image and apply transformation
-  pil_img = Image.open(path)
-  tensor_size1 = (pil_img.size[1], pil_img.size[0])
-  img = transform(np.array(pil_img) * depth_scale)
-  tensor_size2 = img.shape
+    # Load image and apply transformation
+    pil_img = Image.open(path)
+    tensor_size1 = (pil_img.size[1], pil_img.size[0])
+    img = transform(np.array(pil_img) * depth_scale)
+    tensor_size2 = img.shape
 
-  logging.debug(f' - adding {path} with resolution {tensor_size1} --> {tensor_size2}')
-  return img
+    logging.debug(f" - adding {path} with resolution {tensor_size1} --> {tensor_size2}")
+    return img
