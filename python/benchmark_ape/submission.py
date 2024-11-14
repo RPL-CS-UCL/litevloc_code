1ab567f6b89ff315b128ba71b7b662e420eadcc6
gogojjh <jiaojh1994@gmail.com>
2024-11-14 18:02:08 +0000

    add estimator for submission.py

--- python/benchmark_ape/submission.py
+++ python/benchmark_ape/submission.py
@@ -17,8 +17,9 @@ from tqdm import tqdm
 
 from transforms3d.quaternions import mat2quat
 
-# from matching import available_models, get_matcher
-from matching.utils import to_numpy, to_tensor
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../pose_estimation_models"))
+from estimator import available_models, get_estimator
+from estimator.utils import to_numpy, to_tensor
 
 from ape_default import cfg
 from datamodules import DataModule
@@ -27,9 +28,6 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../
 from lib.utils.data import data_to_model_device
 
 from pycpptools.src.python.utils_sensor.utils import correct_intrinsic_scale
-
-def get_ape_method(model, device="cuda"):
-    return model
 
 @dataclass
 class Pose:
@@ -51,66 +49,68 @@ class Pose:
         str_ref_image_names = " ".join(ref_image_name for ref_image_name in self.list_ref_image_name)
         return f"{self.top_K} {self.tar_image_name} {str_ref_image_names} {q_str} {t_str}"
 
-def predict(loader, ape_method, str_ape_metho):
+def predict(loader, estimator, str_estimator):
     results_dict = defaultdict(list)
     running_time = []
     for data in tqdm(loader):
         # try:
-            # data = data_to_model_device(data, ape_method) # in torch.tensor, but in cpu
-            list_ref_img  = [img.squeeze(0) for img in data['list_image0']]
+            data = data_to_model_device(data, estimator) # Probably convert data to cuda
+            list_ref_img = [img.squeeze(0) for img in data['list_image0']] # (3, H, W)
             list_ref_img_K = [K.squeeze(0) for K in data['list_K_color0']]
             list_ref_img_Kori = [K.squeeze(0) for K in data['list_Kori_color0']]
-            list_ref_img_pose = [pose.squeeze(0) for pose in data['list_image0_pose']]          
-            list_ref_img_name = [name[0] for name in data['pair_names'][0]]
-            top_K = data['top_K'].squeeze(0)
-            print(list_ref_img_name)
+            list_ref_img_pose = [pose.squeeze(0) for pose in data['list_image0_pose']]
 
             tar_img = data['image1'].squeeze(0)
             tar_img_K = data['K_color1'].squeeze(0)
             tar_img_Kori = data['Kori_color1'].squeeze(0)
-            tar_img_name = data['pair_names'][1][0]
+            init_img1_pose = data['image1_pose'].squeeze(0)
+
+            print(data['pair_names'][1], data['pair_names'][0])
 
             """Absolute Pose Estimation"""
             start_time = time.time()
-            # ape_result = ape_method(list_ref_img, list_ref_img_K, list_ref_img_pose, tar_img, tar_img_K)
-            ape_result = {"R": to_tensor(np.eye(3), 'cpu'), "t": to_tensor(np.zeros(3), 'cpu')}
-            ape_time = time.time() - start_time
-            running_time.append(ape_time)
+            est_result = estimator(list_ref_img, tar_img, list_ref_img_pose, init_img1_pose, list_ref_img_K, tar_img_K, option='single')
+            est_time = time.time() - start_time
+            running_time.append(est_time)
 
             """Definition of solver output"""
             # Rwc (numpy.ndarray): Estimated rotation matrix from world (reference frame) to camera
             # twc (numpy.ndarray): Estimated translation vector. Shape: [3, 1] that translate depth_img1 to depth_img0.
             # inliers_solver (int): Number of inliers used in the final pose estimation.
-            R, t = ape_result["R"].squeeze(0), ape_result["t"].squeeze(0)
-            Rwc, twc = R, t
-            # inliers = ape_result["inliers"].squeeze(0)
+            im_pose, loss = est_result["im_pose"], est_result["loss"]
+            Rwc, twc = im_pose[:3, :3], im_pose[:3, 3].reshape(3, 1)
             Twc = np.eye(4); Twc[:3, :3] = Rwc; Twc[:3,  3] = twc.reshape(3)
-            Tcw = np.linalg.inv(Twc); R = Twc[:3, :3]; t = Twc[:3,  3].reshape(3, 1)
+            Tcw = np.linalg.inv(Twc); Rcw = Tcw[:3, :3]; tcw = Tcw[:3,  3].reshape(3, 1)
 
             """Save Results"""
             scene = data['scene_id'][0]
-            if np.isnan(R).any() or np.isnan(t).any() or np.isinf(t).any():
+            if np.isnan(im_pose).any():
                 raise ValueError("Estimated pose is NaN or infinite.")
 
             # populate results_dict
+            top_K = data['top_K'].detach().cpu().item()
+            list_ref_img_name = [name[0] for name in data['pair_names'][0]]
+            tar_img_name = data['pair_names'][1][0]
+
             estimated_pose = Pose(top_K=top_K,
-                                list_ref_image_name=list_ref_img_name, 
-                                tar_image_name=tar_img_name,
-                                q=mat2quat(R).reshape(-1),
-                                t=t.reshape(-1))
+                                  list_ref_image_name=list_ref_img_name, 
+                                  tar_image_name=tar_img_name,
+                                  q=mat2quat(Rcw).reshape(-1),
+                                  t=tcw.reshape(-1))
             results_dict[scene].append(estimated_pose)
 
-            # if args.debug:
-            #     print(t.T)
-            #     if num_inliers < 100:
-            #         print(f"Inliers number < 100: {num_inliers} at {data['scene_id'][0]}/{data['pair_names']}")
-            #     out_match_dir = Path(os.path.join(args.out_dir, f"{str_matcher}_{str_solver}"))
-            #     out_match_dir.mkdir(parents=True, exist_ok=True)
-            #     Path(out_match_dir / "preds").mkdir(parents=True, exist_ok=True)
-            #     text = f"{len(mkpts1)} matches: {scene}-{query_img.split('/')[1]}" # "N matches: s00000-frame_000000.jpg"
-            #     save_visualization(rgb_img0, rgb_img1, mkpts0, mkpts1, out_match_dir, save_indice, n_viz=30, line_width=0.6, text=text)
-            #     save_indice += 1
-            #     if str_matcher == "duster" and args.viz: matcher.scene.show(cam_size=0.05)
+
+            if args.debug:
+                print(tcw.T)
+                if args.viz:
+                    estimator.scene.show()
+                # out_match_dir = Path(os.path.join(args.out_dir, f"{str_matcher}_{str_solver}"))
+                # out_match_dir.mkdir(parents=True, exist_ok=True)
+                # Path(out_match_dir / "preds").mkdir(parents=True, exist_ok=True)
+                # text = f"{len(mkpts1)} matches: {scene}-{query_img.split('/')[1]}" # "N matches: s00000-frame_000000.jpg"
+                # save_visualization(rgb_img0, rgb_img1, mkpts0, mkpts1, out_match_dir, save_indice, n_viz=30, line_width=0.6, text=text)
+                # save_indice += 1
+                # if str_matcher == "duster" and args.viz: matcher.scene.show(cam_size=0.05)
         # except Exception as e:
         #     # scene = data['scene_id'][0]
         #     # query_img = data['pair_names'][1][0]
@@ -147,10 +147,10 @@ def eval(args):
     output_root.mkdir(parents=True, exist_ok=True)
     with open(output_root / "runtime_results.txt", "w") as f:
         # for model in args.models:
-        for model in ['master']:
-            ape_method = get_ape_method(model, device=args.device)
+        for model in args.models:
+            estimator = get_estimator(model, device=args.device)
             print(f"Running APE Method: {model}")
-            results_dict, avg_runtime = predict(dataloader, ape_method, model)
+            results_dict, avg_runtime = predict(dataloader, estimator, model)
 
             # Save runtimes to txt
             runtime_str = f"{model}: {avg_runtime:.3f}s"
@@ -165,13 +165,13 @@ def eval(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", help="path to config file")
-    # parser.add_argument(
-    #     "--models",
-    #     type=str,
-    #     nargs="+",
-    #     default="all",
-    #     choices=available_models
-    # )
+    parser.add_argument(
+        "--models",
+        type=str,
+        nargs="+",
+        default="all",
+        choices=available_models
+    )
     # parser.add_argument(
     #     "--pose_solvers",
     #     type=str,
@@ -208,8 +208,6 @@ if __name__ == "__main__":
         help="Dataset split to use for evaluation. Choose from test or val. Default: test",
     )
     args = parser.parse_args()
-    # if args.models == "all":
-    #     args.models = available_models
-    # if args.pose_solvers == "all":
-    #     args.pose_solvers = available_solvers
+    if args.models == "all":
+        args.models = available_models
     eval(args)
