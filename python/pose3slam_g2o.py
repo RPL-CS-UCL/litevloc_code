d66ca06cfd086e258e342ec8eed4bf58dbb4dcdd
gogojjh <jiaojh1994@gmail.com>
2024-11-28 12:41:55 +0000

    add pose3slam_g2o

---
+++ python/pose3slam_g2o.py
@@ -0,0 +1,79 @@
+"""
+ * @file Pose3SLAMExample_initializePose3.cpp
+ * @brief A 3D Pose SLAM example that reads input from g2o, and initializes the
+ *  Pose3 using InitializePose3
+ * @date Jan 17, 2019
+ * @author Vikrant Shah based on CPP example by Luca Carlone
+"""
+# pylint: disable=invalid-name, E1101
+
+from __future__ import print_function
+
+import argparse
+
+import gtsam
+import matplotlib.pyplot as plt
+import numpy as np
+from gtsam.utils import plot
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def vector6(x, y, z, a, b, c):
+    """Create 6d double numpy array."""
+    return np.array([x, y, z, a, b, c], dtype=float)
+
+def parser_arugments():
+    parser = argparse.ArgumentParser(
+        description="A 3D Pose SLAM example that reads input from g2o, and "
+        "initializes Pose3")
+    parser.add_argument('-i', '--input', help='input file g2o format')
+    parser.add_argument('-o', '--output', help="the path to the output file with optimized graph")
+    parser.add_argument("-p", "--plot", action="store_true", help="Flag to plot results")
+    args = parser.parse_args()
+    return args
+
+def main():
+    """Main runner."""
+    args = parser_arugments()
+    g2o_file = args.input
+
+    is3D = True
+    graph, initial = gtsam.readG2o(g2o_file, is3D)
+
+    # Add Prior on the first key
+    print("Adding prior to g2o file ")
+    priorModel = gtsam.noiseModel.Diagonal.Variances(vector6(1e-6, 1e-6, 1e-6, 1e-4, 1e-4, 1e-4))
+    firstKey = initial.keys()[0]
+    graph.add(gtsam.PriorFactorPose3(firstKey, gtsam.Pose3(), priorModel))
+
+    # Set up the optimizer
+    params = gtsam.LevenbergMarquardtParams()
+    params.setVerbosity("Termination")  # this will show info about stopping conds
+    optimizer = gtsam.LevenbergMarquardtOptimizer(graph, initial, params)
+    result = optimizer.optimize()
+    # Output the results
+    print("Optimization complete")
+    print("initial error = ", graph.error(initial))
+    print("final error = ", graph.error(result))
+
+    if args.output is None:
+        print("Final Result:\n{}".format(result))
+    else:
+        outputFile = args.output
+        print("Writing results to file: ", outputFile)
+        graphNoKernel, _ = gtsam.readG2o(g2o_file, is3D)
+        gtsam.writeG2o(graphNoKernel, result, outputFile)
+        print("Done!")
+
+    if args.plot:
+        resultPoses = gtsam.utilities.allPose3s(result)
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        for i in range(resultPoses.size()):
+            plot.plot_pose3(1, resultPoses.atPose3(i))
+        ax.view_init(elev=0, azim=90)
+        plt.tight_layout()
+        plt.show()
+
+if __name__ == "__main__":
+    main()
