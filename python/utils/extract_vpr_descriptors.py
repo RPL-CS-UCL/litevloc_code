195dad3465afd1bb65eb4efe48321a9a5c50e3aa
gogojjh <jiaojh1994@gmail.com>
2025-04-17 20:08:46 +0100

    add feat: update test settings for vineyard and hkust

--- python/utils/extract_vpr_descriptors.py
+++ python/utils/extract_vpr_descriptors.py
@@ -36,39 +36,43 @@ if not hasattr(sys, "ps1"):
 
 def main(args):
 	"""Main function to run the image matching process."""
-	out_dir = Path(os.path.join(args.dataset_path, 'output_extract_vpr_descriptors'))
-	out_dir.mkdir(exist_ok=True, parents=True)
-	log_dir = setup_log_environment(out_dir, args)
+	for scene in sorted(os.listdir(args.dataset_path)):
+		scene_path = os.path.join(args.dataset_path, scene)
+		out_dir = scene_path
 
-	"""Initialize VPR model"""
-	model = initialize_vpr_model(args.method, args.backbone, args.descriptors_dimension, args.device)
+		# out_dir.mkdir(exist_ok=True, parents=True)
+		# log_dir = setup_log_environment(out_dir, args)
 
-	"""Load images"""
-	image_graph = ImageGraphLoader.load_data(
-		args.dataset_path, 
-		args.image_size, 
-		depth_scale=0.001, 
-		load_rgb=True, 
-		load_depth=False, 
-		normalized=True
-	)
+		"""Initialize VPR model"""
+		model = initialize_vpr_model(args.method, args.backbone, args.descriptors_dimension, args.device)
 
-	# Extract VPR descriptors for all nodes in the map
-	start_time = time.time()
-	db_descriptors_id = image_graph.get_all_id()
-	db_descriptors = np.empty((image_graph.get_num_node(), args.descriptors_dimension + 1), dtype=object)
-	for indices, (_, map_node) in enumerate(image_graph.nodes.items()):
-		with torch.no_grad():
-			desc = model(map_node.rgb_image.unsqueeze(0).to(args.device)).cpu().numpy()
-			vec = np.empty((1, args.descriptors_dimension + 1), dtype=object)
-			vec[0, 0], vec[0, 1:] = map_node.rgb_img_name, desc[0]
-			db_descriptors[indices, :] = vec[0, :]
-	print(f"IDs: {db_descriptors_id} extracted {db_descriptors.shape} VPR descriptors.")
-	print(f'Extract each VPR descriptor costs: {(time.time() - start_time) / len(db_descriptors):.3f}s')
+		"""Load images"""
+		image_graph = ImageGraphLoader.load_data(
+			Path(scene_path), 
+			args.image_size, 
+			depth_scale=0.001, 
+			load_rgb=True, 
+			load_depth=False, 
+			normalized=True
+		)
 
-	"""Save image descriptors"""
-	if args.save_descriptors:
-		np.savetxt(os.path.join(log_dir, 'preds', f'database_descriptors.txt'), db_descriptors, fmt='%s ' + '%.9f ' * args.descriptors_dimension)
+		# Extract VPR descriptors for all nodes in the map
+		start_time = time.time()
+		db_descriptors_id = image_graph.get_all_id()
+		db_descriptors = np.empty((image_graph.get_num_node(), args.descriptors_dimension + 1), dtype=object)
+		for indices, (_, map_node) in enumerate(image_graph.nodes.items()):
+			with torch.no_grad():
+				desc = model(map_node.rgb_image.unsqueeze(0).to(args.device)).cpu().numpy()
+				vec = np.empty((1, args.descriptors_dimension + 1), dtype=object)
+				vec[0, 0], vec[0, 1:] = map_node.rgb_img_name, desc[0]
+				db_descriptors[indices, :] = vec[0, :]
+		print(f"IDs: {db_descriptors_id} extracted {db_descriptors.shape} VPR descriptors.")
+		print(f'Extract each VPR descriptor costs: {(time.time() - start_time) / len(db_descriptors):.3f}s')
+
+		"""Save image descriptors"""
+		if args.save_descriptors:
+			print(f"Saving image descriptors to {out_dir}")
+			np.savetxt(os.path.join(out_dir, f'database_descriptors.txt'), db_descriptors, fmt='%s ' + '%.9f ' * args.descriptors_dimension)
 
 if __name__ == "__main__":
 	args = parse_arguments()
