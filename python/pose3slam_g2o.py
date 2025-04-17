f0cb4ad0cb2daefc3dd8b1e8894147f87295b5ca
gogojjh <jiaojh1994@gmail.com>
2025-04-17 17:07:55 +0100

    add feat: robst estimatior for pose graph optimization

--- python/pose3slam_g2o.py
+++ python/pose3slam_g2o.py
@@ -14,30 +14,7 @@ import numpy as np
 import gtsam
 import time
 from utils.gtsam_pose_graph import PoseGraph
-
-# Using GTSAM
-def optimize_pose_graph(graph, initial, verbose=False):
-    """
-    Optimizes a pose graph using the Levenberg-Marquardt algorithm.
-
-    This function adds a prior factor to the first key in the initial estimate to anchor the graph,
-    then optimizes the graph to minimize the error.
-
-    Args:
-        graph (gtsam.NonlinearFactorGraph): The pose graph containing factors (constraints).
-        initial (gtsam.Values): Initial estimates for the variables (poses) in the graph.
-
-    Returns:
-        gtsam.Values: The optimized values (poses) after the optimization process.
-    """    
-    # Set up the optimizer
-    params = gtsam.LevenbergMarquardtParams()
-    if verbose:
-        params.setVerbosity("Termination")
-    optimizer = gtsam.LevenbergMarquardtOptimizer(graph, initial, params)
-    result = optimizer.optimize()
-    
-    return result
+import copy
 
 def vector6(x, y, z, a, b, c):
     """Create 6d double numpy array."""
@@ -59,6 +36,7 @@ def main():
     args = parser_arugments()
     g2o_file = args.input
 
+    ##### Load an exiting G2O file
     is3D = True
     graph, initial = gtsam.readG2o(g2o_file, is3D)
     print(f"Graph Info: ---------------------")
@@ -66,37 +44,47 @@ def main():
     print(f"Number of variables: {len(graph.keyVector())}")
 
     # Add prior factor on each disconnected graph
-    priorModel = gtsam.noiseModel.Diagonal.Variances(vector6(1e-6, 1e-6, 1e-6, 1e-4, 1e-4, 1e-4))
-    
+    # Orientation and Translation Variance
+    priorModel = gtsam.noiseModel.Diagonal.Sigmas(np.array([np.deg2rad(1.0)] * 3 + [0.1] * 3) / 1e2)
+
     start_time = time.time()
     comp_graph_keys = PoseGraph.find_connected_components(graph)
-    print(f"Search connected components costs time: {time.time() - start_time:.3f}s")
+    print(f"----- Search connected components costs: {time.time() - start_time:.3f}s")
     for comp_id, comp_keys in enumerate(comp_graph_keys):
         init_estimate = initial.atPose3(comp_keys[0])
         graph.add(gtsam.PriorFactorPose3(comp_keys[0], init_estimate, priorModel))
         print(f"Add prior factor: {comp_keys[0]} to the {comp_id} subgraph with node number {len(comp_keys)}")
 
+    ##### Optimize the graph
     if args.viz:
         result = initial
         print("initial error = ", graph.error(initial))
+        if args.plot:
+            PoseGraph.plot_pose_graph(None, graph, [initial, initial], ['Before PGO', 'Before PGO'], mode='3d')
     else:
-        result = optimize_pose_graph(graph, initial, True)
+        start_time = time.time()
+        result = PoseGraph.optimize_pose_graph_with_LM(
+            graph, initial, 
+            verbose=True, 
+            robust_kernel=True
+        )
+        print(f"----- Optimization costs: {time.time() - start_time:.3f}s")
         print("Optimization complete")
         print("initial error = ", graph.error(initial))
         print("final error = ", graph.error(result))
 
-    if args.output is None:
-        # print("Final Result:\n{}".format(result))
-        pass
-    else:
-        outputFile = args.output
-        print("Writing results to file: ", outputFile)
-        graphNoKernel, _ = gtsam.readG2o(g2o_file, is3D)
-        gtsam.writeG2o(graphNoKernel, result, outputFile)
-        print("Done!")
+        if args.output is None:
+            # print("Final Result:\n{}".format(result))
+            pass
+        else:
+            outputFile = args.output
+            print("Writing results to file: ", outputFile)
+            graphNoKernel, _ = gtsam.readG2o(g2o_file, is3D)
+            gtsam.writeG2o(graphNoKernel, result, outputFile)
+            print("Done!")
 
-    if args.plot:
-        PoseGraph.plot_pose_graph(None, graph, result, mode='3d')
+        if args.plot:
+            PoseGraph.plot_pose_graph(None, graph, [initial, result], ['Before PGO', 'After PGO'], mode='3d')
 
 if __name__ == "__main__":
     main()
